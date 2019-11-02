a0a998e809f3211e7d1bb0bb1ebb2045d0a89656
Harshareddy <harshavardhan819@gmail.com>
2019-11-02 13:52:37 +0530

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Sunny Reddy on 02/11/19.
+//  Copyright © 2019 Harsha Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
